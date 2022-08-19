750586ebf6cba8ce14355078819b9db335b2f3da
A. Kotnik <ak2164@cam.ac.uk>
2022-08-19 20:50:32 +0100

    changing streamlines file to try to fix the streamlines demo

--- code/dist/streamlines.js
+++ code/dist/streamlines.js
@@ -32615,7 +32615,7 @@
 
 	function text2csv(t) {
 	  // Read a regular table and create json objects based on the rows.
-	  var rows = t.split("\r\n"); // \r\n depending on hte csv file??
+	  var rows = t.split("\n"); // \r\n depending on hte csv file??
 
 	  var header = rows.splice(0, 1)[0];
 	  var properties = header.split(",").map(function (t) {
